5e9a1ef16156c8632a8cb9c810068bd9e2f8c8bc
Rwan <rabdelmonem0@gmail.com>
2021-05-12 13:49:46 -0700

    added routes for category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -35,15 +35,52 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
-  Category.create()
+  Category.create({
+    category_name: req.body.category_name
+  })
+  .then(category => res.json(category))
+  .catch(err => res.status(500).json(err))
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update({
+    category_name: req.body.category_name
+  },
+  {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(category => {
+    if (!category){
+      res.status(404).json({ message: 'There is no category with this ID!' })
+    return;
+    }
+    res.json(category);
+  })
+  .catch(err => {
+    res.status(500).json(err)
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(category => {
+      if (!category) {
+        res.status(404).json({ message: 'There is no category with this ID!' });
+        return;
+      }
+      res.json(category);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
